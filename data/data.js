450c1fc1cd942308e7c1d3ec1ba55e4362376513
marlen200186 <marlen200186@mail.ru>
2020-12-03 18:01:40 +0300

    11

---
+++ data/data.js
@@ -0,0 +1,72 @@
+var test = "Умножение";
+var peremeshat_voprosi = "да";
+var vopros_1 = "";
+var vopros_2 = "";
+var vopros_3 = "";
+var vopros_4 = "";
+var vopros_5 = "";
+var vopros_6 = "";
+var vopros_7 = "";
+var vopros_8 = "";
+var vopros_9 = "";
+var vopros_10 = "";
+var vopros_11 = "";
+var vopros_12 = "";
+var vopros_13 = "";
+var vopros_14 = "";
+var vopros_15 = "";
+var vopros_16 = "";
+var vopros_17 = "";
+var vopros_18 = "";
+var vopros_19 = "";
+var vopros_20 = "";
+var vopros_21 = "";
+var vopros_22 = "";
+var vopros_23 = "";
+var vopros_24 = "";
+var vopros_25 = "";
+var vopros_26 = "";
+var vopros_27 = "";
+var vopros_28 = "";
+var vopros_29 = "";
+var vopros_30 = "";
+
+var vopros_ris_1 = "да";
+var vopros_ris_2 = "да";
+var vopros_ris_3 = "да";
+var vopros_ris_4 = "да";
+var vopros_ris_5 = "да";
+var vopros_ris_6 = "да";
+var vopros_ris_7 = "да";
+var vopros_ris_8 = "да";
+var vopros_ris_9 = "да";
+var vopros_ris_10 = "да";
+var vopros_ris_11 = "да";
+var vopros_ris_12 = "да";
+var vopros_ris_13 = "да";
+var vopros_ris_14 = "да";
+var vopros_ris_15 = "да";
+var vopros_ris_16 = "да";
+var vopros_ris_17 = "да";
+var vopros_ris_18 = "да";
+var vopros_ris_19 = "да";
+var vopros_ris_20 = "да";
+var vopros_ris_21 = "да";
+var vopros_ris_22 = "да";
+var vopros_ris_23 = "да";
+var vopros_ris_24 = "да";
+var vopros_ris_25 = "да";
+var vopros_ris_26 = "да";
+var vopros_ris_27 = "да";
+var vopros_ris_28 = "да";
+var vopros_ris_29 = "да";
+var vopros_ris_30 = "да";
+
+var pr_otv_zadachi1 = ["MjI=", "MjQ=", "MjY=", "Mjg=", "MzA=", 
+                      "MzI=", "MzQ=", "MzY=", "Mzg=", "MjIw"];
+					  
+var pr_otv_zadachi2 = ["NDI=", "NDQ=", "NDY=", "NDg=", "NTA=",
+					  "NTI=", "NTQ=", "NTY=", "NTg=", "NDIw"];
+					  
+var pr_otv_zadachi3 = ["NjI=", "NjQ=", "NjY=", "Njg=", "NzA=",
+					  "NzI=", "NzQ=", "NzY=", "Nzg=", "NjIw"];
