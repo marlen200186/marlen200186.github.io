398412a69dd396d0b1379c48fb667a4c34583ac6
marlen200186 <marlen200186@mail.ru>
2021-01-01 23:02:33 +0300

    49

---
+++ data_10cl/data.js
@@ -0,0 +1,248 @@
+var  kol_zad_vbase1=18;    
+var  kol_zad_vbase2=1;
+var  kol_zad_vbase3=0;
+var  kol_zad_vbase4=34;    
+var  kol_zad_vbase5=0;
+var  kol_zad_vbase6=0;    
+var  kol_zad_vbase7=29;
+var  kol_zad_vbase8=0;
+var  kol_zad_vbase9=0;    
+var  kol_zad_vbase10=0;
+var  kol_zad_vbase11=20;    
+var  kol_zad_vbase12=0;
+var  kol_zad_vbase13=14;
+var  kol_zad_vbase14=0;    
+var  kol_zad_vbase15=0;
+var  kol_zad_vbase16=0;    
+var  kol_zad_vbase17=18;
+var  kol_zad_vbase18=0;
+var  kol_zad_vbase19=0;    
+var  kol_zad_vbase20=0;
+var  kol_zad_vbase21=0;    
+var  kol_zad_vbase22=0;
+var  kol_zad_vbase23=0;
+var  kol_zad_vbase24=0;    
+var  kol_zad_vbase25=0;
+var  kol_zad_vbase26=0;    
+var  kol_zad_vbase27=0;
+
+var  kol_zad_vbase10_1=25;    
+var  kol_zad_vbase10_2=0;
+var  kol_zad_vbase10_3=0;
+var  kol_zad_vbase10_4=0;    
+var  kol_zad_vbase10_5=0;
+var  kol_zad_vbase10_6=0;    
+var  kol_zad_vbase10_7=0;
+var  kol_zad_vbase10_8=0;
+var  kol_zad_vbase10_9=0;    
+var  kol_zad_vbase10_10=0;
+var  kol_zad_vbase10_11=0;    
+var  kol_zad_vbase10_12=0;
+var  kol_zad_vbase10_13=0;
+var  kol_zad_vbase10_14=0;    
+var  kol_zad_vbase10_15=0;
+var  kol_zad_vbase10_16=0;    
+var  kol_zad_vbase10_17=0;
+var  kol_zad_vbase10_18=0;
+var  kol_zad_vbase10_19=0;    
+var  kol_zad_vbase10_20=0;
+
+
+var test = "ЕГЭ";
+var peremeshat_voprosi = "да";
+var vopros_1 = "";
+var vopros_2 = "";
+var vopros_3 = "";
+var vopros_4 = "";
+var vopros_5 = "";
+var vopros_6 = "";
+var vopros_7 = "";
+var vopros_8 = "";
+var vopros_9 = "";
+var vopros_10 = "";
+var vopros_11 = "";
+var vopros_12 = "";
+var vopros_13 = "";
+var vopros_14 = "";
+var vopros_15 = "";
+var vopros_16 = "";
+var vopros_17 = "";
+var vopros_18 = "";
+var vopros_19 = "";
+var vopros_20 = "";
+var vopros_21 = "";
+var vopros_22 = "";
+var vopros_23 = "";
+var vopros_24 = "";
+var vopros_25 = "";
+var vopros_26 = "";
+var vopros_27 = "";
+var vopros_28 = "";
+var vopros_29 = "";
+var vopros_30 = "";
+var vopros_31 = "";
+var vopros_32 = "";
+var vopros_33 = "";
+var vopros_34 = "";
+var vopros_35 = "";
+var vopros_36 = "";
+var vopros_37 = "";
+var vopros_38 = "";
+var vopros_39 = "";
+var vopros_40 = "";
+var vopros_41 = "";
+var vopros_42 = "";
+var vopros_43 = "";
+var vopros_44 = "";
+var vopros_45 = "";
+var vopros_46 = "";
+var vopros_47 = "";
+var vopros_48 = "";
+var vopros_49 = "";
+var vopros_50 = "";
+
+var vopros_ris_1 = "да";
+var vopros_ris_2 = "да";
+var vopros_ris_3 = "да";
+var vopros_ris_4 = "да";
+var vopros_ris_5 = "да";
+var vopros_ris_6 = "да";
+var vopros_ris_7 = "да";
+var vopros_ris_8 = "да";
+var vopros_ris_9 = "да";
+var vopros_ris_10 = "да";
+var vopros_ris_11 = "да";
+var vopros_ris_12 = "да";
+var vopros_ris_13 = "да";
+var vopros_ris_14 = "да";
+var vopros_ris_15 = "да";
+var vopros_ris_16 = "да";
+var vopros_ris_17 = "да";
+var vopros_ris_18 = "да";
+var vopros_ris_19 = "да";
+var vopros_ris_20 = "да";
+var vopros_ris_21 = "да";
+var vopros_ris_22 = "да";
+var vopros_ris_23 = "да";
+var vopros_ris_24 = "да";
+var vopros_ris_25 = "да";
+var vopros_ris_26 = "да";
+var vopros_ris_27 = "да";
+var vopros_ris_28 = "да";
+var vopros_ris_29 = "да";
+var vopros_ris_30 = "да";
+var vopros_ris_31 = "да";
+var vopros_ris_32 = "да";
+var vopros_ris_33 = "да";
+var vopros_ris_34 = "да";
+var vopros_ris_35 = "да";
+var vopros_ris_36 = "да";
+var vopros_ris_37 = "да";
+var vopros_ris_38 = "да";
+var vopros_ris_39 = "да";
+var vopros_ris_40 = "да";
+var vopros_ris_41 = "да";
+var vopros_ris_42 = "да";
+var vopros_ris_43 = "да";
+var vopros_ris_44 = "да";
+var vopros_ris_45 = "да";
+var vopros_ris_46 = "да";
+var vopros_ris_47 = "да";
+var vopros_ris_48 = "да";
+var vopros_ris_49 = "да";
+var vopros_ris_50 = "да";
+
+//старое первое задание на системы счисления
+//1_2,1_3,1_13,1_14,1_15,1_22,1_23,1_24,1_25,1_27,
+//1_40,1_43,1_45,1_47,1_48,1_55,1_73,1_74,1_77,1_79,
+//1_84,1_85,1_86,1_91,1_94
+var pr_otv_zadachi10_1 = ["NA==","NQ==","NA==","Mw==","Mw==",
+                       "MQ==","Mw==","Mg==","NA==","Mw==",
+                       "MQ==","Mw==","Mg==","MQ==","Mg==",
+					   "Mw==","NQ==","NA==","NQ==","NA==",
+					   "Mw==","NQ==","Ng==","MTU=","MTgx"];
+
+
+
+
+
+
+
+
+//1_49,1_50,1_56,1_59,1_61, 1_63,1_73, 1_77, 1_80,1_83,
+//1_86,1_87, 1_89, 1_90,1_94, 1_96, 1_98,1_101
+var pr_otv_zadachi1 = ["OA==", "MTI=", "MTY=", "MjI=", "MzA=", 
+                      "OA==", "NQ==", "MjA=", "MTQ=", "MjU=",
+					  "Nw==","MjU=","MjU=","MzA=","Njc=",
+					  "QTUW","Q==","MjY="];
+
+//старое первое задание на системы счисления
+//1_2,1_3,1_13,1_14,1_15,1_22,1_23,1_24,1_25,1_27,
+//1_40,1_43,1_45,1_47,1_48,1_55,1_73,1_74,1_77,1_79,
+//1_84,1_85,1_86,1_91,1_94
+var pr_otv_zadachi2 = ["NA==","NQ==","NA==","Mw==","Mw==",
+                       "MQ==","Mw==","Mg==","NA==","Mw==",
+                       "MQ==","Mw==","Mg==","MQ==","Mg==",
+					   "Mw==","NQ==","NA==","NQ==","NA==",
+					   "Mw==","NQ==","Ng==","MTU=","MTgx"];
+
+
+//3_58,3_59,3_61,3_64,3_65,3_66,3_68, 3_70, 3_72,3_74
+//3_78,3_79,3_81,3_82,3_83,3_84,3_87,3_88,3_90,3_91,
+//3_89,3_85,3_80
+var pr_otv_zadachi3 = ["Mw==","Nw==","NjIxNA==","MzY4OA==","Njk1Mg==",
+						"Mw==","NA==","Mw==","Mw==","Mw==",
+						"MQ==","MA==","Mg==","MA==","Mw==",
+						"Mg==","Mw==","NQ==","MjE4Ng==","MjA4MQ==",
+                        "Ng==","NTE3","Mg=="];
+
+
+  
+//4_40,4_41,4_42,4_44,4_45,4_52,4_55,4_87,4_88,4_91,
+//4_94,4_110,4_111,4_112,4_114,4_115,4_116,4_117,4_123,4_124,
+//4_125,4_126,4_127,4_128,4_129,4_130,4_139,4_140,4_141,4_142,
+//4_143,4_144,4_148,4_150 
+var pr_otv_zadachi4 = ["Mg==", "MQ==", "Mw==", "NA==", "Mw==",
+					  "Mw==", "Mg==", "MDA=", "Mg==", "MQ==",
+					  "Mw==","MjU=","MjM=","MjA=","MDAw",
+					  "MTEw","MTAw","MTEwMA==","MTAxMQ==","MDEw",
+					  "MDEx","MTAx","MDAwMA==","MTAwMA==","MDA=",
+					  "MTA=","MTQ=","MTM=","MTU=","MTI=",
+					  "MTEx","NQ==","Ng==","MjM="];
+					  
+//7_1(24),7_1(25),7_1(27),7_1(28),7_1(30),7_1(40),7_1(46),7_1(65),7_1(66),7_2(36),
+//7_2(37),7_2(38),7_2(39),7_2(40),7_2(48),7_2(49),7_2(55),7_2(67),7_2(69),7_2(70), 
+//7_2(78),7_v(23),7_v(24),7_v(40), 7v(43),7_v(44),7_v(49),7_v(51),7_v(70)
+var pr_otv_zadachi7 = ["MjQ=", "NjQ=", "NzI=", "NDI=", "MzI=", 
+					  "OA==", "MTA=", "MzAw", "MjU2", "OTA=",
+					  "NDA=", "NA==", "Mw==", "Mg==", "ODA=",
+					  "OA==", "MTg=", "NTA=", "NA==", "NDI=",
+					  "NQ==","MTA4OA==", "MTA1Ng==", "MTAyNDg=", "DEyMA==",   
+					  "TY=", "MjMwNzA=","Ng==","TE0"];	
+					  
+//11_34	,11_35, 11_39,11_46,11_47,11_48,11_51,11_52,11_53,11_57,
+//11_58,11_67,11_65,11_66,11_62,11_63,11_61, 11_59,11_54,11_56
+var pr_otv_zadachi11 = ["Mjg1MA==", "MTMwMA==", "OTQ=", "MTAwMA==", "Mzkw",
+					  "NDAw", "OA==", "MTA=", "MTI=", "MTEz",
+					  "NDA=","MTg=","NDE=","MjM=","MjMwMA==",
+					  "MTY=","MjM=","NDY=","MjE=","MTg2"];
+
+//13_33,13_36,13_37,13_40,13_53,13_54,13_86,13_84,13_83,13_82
+//13_81,13_79,13_77,13,71,
+var pr_otv_zadachi13 = ["MzQ=","MzY=","Nw==","MjQ=","OQ==",
+						"MTA=","Mjc=","MTA=", "MTE=","MTE=",
+						"Nw==","MjU=","MzA=","MTQ0"]
+
+//17_1,17_2, 17_3, 17_4, 17_5, 17_6, 17_7, 17_23, 17_24, 17_25,
+//17_26, 17_35, 17_36,17_37,17_38,17_45,17_47, 17_48
+var pr_otv_zadachi17 = ["MjE1MSA5NjMw","MTY0OSAxMjg3Ng==","MTE3OCAxMDk5Mg==","MTI2NSAxMzE4OA==","NTE5IDk2ODA=",
+						"MTQxNCAxMjAw","Nzc2IDIwMA==","MzIzMiA4Mjk4","MjU2OCA3ODU2","MzYzIDIzMTkyMDc=",
+						"ODU4NyAxNDg2Mzg4","MTQ1NSA0MjM3","MTYyMiA1MTM3","MTYwMCA0MjEw","MjkxOSA5Njc5",
+						"NzIzIC0zMTE1","MjA1NyAxOTI=","ODUxNSAxMjU="]
+
+
+var identificatori  = [['11','Иванов'], ['07691281','Брюхов'], ['07604704','Ваганов'], ['0765825','Голомако'],['0760816','Емелин'],
+                       ['0766339','Калякин'],['0764957','Курицын'],['0761851','Лыжин'],['0765331','Тарасенко'],['07619845','Шустров'],
+					   ['0765094','Буров'],['0764965','Ершов'],['0760925','Коротина'],['0764295','Лебедев'],['0767207','Матухина'],
+					   ['0768853','Москалев'],['075389','Суслов'],['0763965','Черякин'],['0763891','Шишкова'],['0764378', 'Толстикова']];					  
+					  
