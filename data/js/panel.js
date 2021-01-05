eff25b453318bacd7b581818b797c9d1f2f1ee51
marlen200186 <marlen200186@mail.ru>
2021-01-05 16:14:40 +0300

    2

--- data/js/panel.js
+++ data/js/panel.js
@@ -32,7 +32,7 @@ inpKol1=document.getElementById('kol1').valueAsNumber;
   inpKol26=document.getElementById('kol26').valueAsNumber;
   inpKol27=document.getElementById('kol27').valueAsNumber;
 
-  inpKol=+inpKol1+ +inpKol2+ +inpKol3+ +inpKol4+ +inpKol5+ +inpKol6+ +inpKol7+ +inpKol8+ +inpKol9+ +inpKol10+ +inpKol11+ +inpKol12+ +inpKol13+ +inpKol14+ +inpKol15+ +inpKol6+ +inpKol17+ +inpKol18+ +inpKol19+ +inpKol20+ +inpKol21+ +inpKol22+ +inpKol23+ +inpKol24+ +inpKol25+ +inpKol26+ +inpKol27 ;     //количество заданий из формы
+  inpKol=+inpKol1+ +inpKol2+ +inpKol3+ +inpKol4+ +inpKol5+ +inpKol6+ +inpKol7+ +inpKol8+ +inpKol9+ +inpKol10+ +inpKol11+ +inpKol12+ +inpKol13+ +inpKol14+ +inpKol15+ +inpKol16+ +inpKol17+ +inpKol18+ +inpKol19+ +inpKol20+ +inpKol21+ +inpKol22+ +inpKol23+ +inpKol24+ +inpKol25+ +inpKol26+ +inpKol27 ;     //количество заданий из формы
      //количество заданий из формы
   numberZad=document.getElementById('kolich').value;
   
