17b2dfb1a85d9c8d7a6802f6ecc0e9b558fe7001
marlen200186 <marlen200186@mail.ru>
2021-01-22 00:48:49 +0300

    1

---
+++ 2.php
@@ -0,0 +1,11 @@
+  <?php
+  function ucs2toutf8{
+			   $text = "ааооогг";
+
+                 echo 'Original : ', $text, PHP_EOL;
+                 echo 'TRANSLIT : ', iconv("UTF-8", "ISO-8859-1//TRANSLIT", $text), PHP_EOL;
+				echo 'IGNORE   : ', iconv("UTF-8", "ISO-8859-1//IGNORE", $text), PHP_EOL;
+				echo 'Plain    : ', iconv("UTF-8", "ISO-8859-1", $text), PHP_EOL;
+	}
+echo ucs2toutf8;	
+			   ?>
